34619712e6c4e6cd4dcd995401957a2e9fb4d4df
Jason Brunelle <jasonmb626@gmail.com>
2025-07-12 12:45:18 -0500

    Add high-level integration test to cover basic application behavior

---
+++ test/test_main.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import sys
+from unittest.mock import patch
+from audio_clips_remover import main
+
+
+def test_one_equals_one():
+    input_file_name = "2019-11-02 - Episode 93 - 5x05 Thirst.mp3"
+    with patch.object(sys, 'argv', ['-u', 'Waterhose_commercial.arr.npy', '-u', 'Waterhose_commercial2.arr.npy', input_file_name]):
+        main()
+        actual_output_file = (Path('trimmed') / input_file_name)
+        assert actual_output_file.exists()
+        assert actual_output_file.read_bytes() == (Path('test') / 'resources'/ input_file_name ).read_bytes()

2a0db19063dae18fa92b18a0e13e2cbf8566bf36
Jason Brunelle <jasonmb626@gmail.com>
2025-07-12 16:05:35 -0500

    Add skeleton for Audio Clip Class

--- src/audio_clip.py
+++ src/audio_clip.py
@@ -11,3 +11,32 @@ class AudioClip():
     def __init__(self, audio_data, sample_rate: int):
         self.audio_data = audio_data
         self.sample_rate = sample_rate
+        self.audio_mean = None # TODO: write test for and calculate this useful representation of the data
+
+    @property
+    def clip_length(self):
+        pass
+
+    def downmix_stereo_to_mono(self) -> Self:
+        pass
+
+    def remove_audio_clip(self, start_index, end_index, capture_discard=False) -> tuple[Self, Self]:
+        """
+        Returns a Tuple of (audio clip with segment removed, removed segment)
+        """
+        pass
+
+    def trim_whitespace(self) -> Self:
+        pass
+
+    def save(self, path):
+        pass
+
+    def has_matching_sample_rate(self, other_clip: Self) -> bool:
+        pass
+
+    def prepend(self, other_clip: Self) -> Self:
+        """
+        Used especially for building up a discarded audio file
+        """
+        pass

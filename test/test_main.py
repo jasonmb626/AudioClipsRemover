8e0937a4343e72437e09c3bb1cd86c8e6fb7c6b4
Jason Brunelle <jasonmb626@gmail.com>
2025-07-12 14:05:18 -0500

    Use shorter audio clip in integration test

--- test/test_main.py
+++ test/test_main.py
@@ -4,8 +4,8 @@ from unittest.mock import patch
 from audio_clips_remover import main
 
 
-def test_one_equals_one():
-    input_file_name = "2019-11-02 - Episode 93 - 5x05 Thirst.mp3"
+def test_should_remove_ads_from_short_clip():
+    input_file_name = "2019-11-02 - Episode 93 - 5x05 Thirst-trimmed.mp3"
     with patch.object(sys, 'argv', ['-u', 'Waterhose_commercial.arr.npy', '-u', 'Waterhose_commercial2.arr.npy', input_file_name]):
         main()
         actual_output_file = (Path('trimmed') / input_file_name)
